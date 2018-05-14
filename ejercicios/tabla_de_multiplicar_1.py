8dac9aa28837306c43897d76bde2d11ac667c78d
byalex4321 <38331411+byalex4321@users.noreply.github.com>
2018-05-14 16:48:32 +0200

    Update tabla_de_multiplicar_1.py

--- ejercicios/tabla_de_multiplicar_1.py
+++ ejercicios/tabla_de_multiplicar_1.py
@@ -1,5 +1,5 @@
 
 numero_tabla = int(input("¿De que numero quieres hacer la tabla?:"))
 
-for multiplo in range(5, 16):
+for multiplo in range(1, 16):
     print("{} * {} = {}".format(multiplo, numero_tabla, multiplo*numero_tabla))
