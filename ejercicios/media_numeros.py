c78f50163df8d6360b1f2565a146e8dc8864be85
alex <holagtanianos@gmail.com>
2018-05-15 20:19:06 +0200

    ejercicios

--- ejercicios/media_numeros.py
+++ ejercicios/media_numeros.py
@@ -1,11 +1,15 @@
 
 numeros_usuario = []
-suma_numeros_usuario = ""
-numeros_lista = len(numeros_usuario)
+suma_numeros_usuario = 0
+numeros_lista = 0
+parar_pregunta = "F"
+pregunta = "N"
 
-
-
-    
-
-
-
+while pregunta != parar_pregunta:
+    pregunta = input("Dime un numero, pon F si quieres dejar de añadir numeros:")
+    if pregunta == parar_pregunta:
+        print("El resultado de {} / {} = {}".format(suma_numeros_usuario, numeros_lista, suma_numeros_usuario/numeros_lista ))
+    elif pregunta.isdigit():
+        suma_numeros_usuario += int(pregunta)
+        numeros_usuario.append(pregunta)
+        numeros_lista += 1
